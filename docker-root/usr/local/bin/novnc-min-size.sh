79db027628715a4c7c21f1aa0d50fb4efdcd5ecc
zhjcreator <55911606+zhjcreator@users.noreply.github.com>
2026-07-14 21:21:50 +0800

    fix: 简化 noVNC 架构，用 websockify --web 替代 tinyproxy+busybox httpd
    - tinyproxy 1.8.3 (Ubuntu 18.04) 不支持 WebSocket 升级转发
    - 改用 websockify --web 同时提供静态文件服务和 WebSocket 代理
    - 移除 tinyproxy 反向代理和 busybox httpd 两层中间件
    - noVNC 访问路径: http://host:8080/vnc.html

--- docker-root/usr/local/bin/novnc-min-size.sh
+++ docker-root/usr/local/bin/novnc-min-size.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-su daemon -s /bin/sh -c "exec websockify --daemon 127.0.0.1:8082 127.0.0.1:5901"
-busybox httpd -u daemon:daemon -p 127.0.0.1:8081 -h /usr/local/share/novnc
-exec tinyproxy -c /etc/tinyproxy-novnc.conf
+exec su daemon -s /bin/sh -c "exec websockify --web /usr/local/share/novnc 0.0.0.0:8080 127.0.0.1:5901"
